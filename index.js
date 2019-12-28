b28b2d4e174c85f032ce6bd5027517bf1ca17782
Douglas Pires <douglas.pires@live.com>
2019-12-28 01:04:23 -0300

    add credentials as parameter

--- index.js
+++ index.js
@@ -8,9 +8,8 @@ const {
   waitFor,
 } = require("taiko");
 const fs = require("fs");
-const credentials = require('./credentials.json')
 
-const init = async (path) => {
+const init = async (path, credentials) => {
   await openBrowser({ headless: false, args: ["--window-size=1440,900"] });
   await goto(
     "https://www.amazon.com.br/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=brflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.br%2F%3Fref_%3Dnav_signin&switch_account="
