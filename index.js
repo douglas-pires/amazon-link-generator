e7dd6b548af4a8fb049af7d19094ecd7a8d6a42e
Douglas Pires <douglas.pires@live.com>
2019-12-28 00:59:08 -0300

    small tweaks

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const {
 const fs = require("fs");
 const credentials = require('./credentials.json')
 
-const init = async () => {
+const init = async (path) => {
   await openBrowser({ headless: false, args: ["--window-size=1440,900"] });
   await goto(
     "https://www.amazon.com.br/ap/signin?_encoding=UTF8&ignoreAuthState=1&openid.assoc_handle=brflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com.br%2F%3Fref_%3Dnav_signin&switch_account="
@@ -47,7 +47,7 @@ const init = async () => {
   const nextStep = async products => {
     await waitFor(2000);
     fs.writeFileSync(
-      `products/products_${new Date().getTime()}.json`,
+      `${path}/products_${new Date().getTime()}.json`,
       JSON.stringify(products)
     );
     try {
